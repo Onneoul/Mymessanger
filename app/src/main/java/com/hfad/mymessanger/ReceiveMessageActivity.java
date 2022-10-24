d37fcc4ab8e3580b24ed857eb4016cb8a5df1333
Onneoul <kimminse99@naver.com>
2022-10-24 15:40:34 +0900

    1024 Commit

---
+++ app/src/main/java/com/hfad/mymessanger/ReceiveMessageActivity.java
@@ -0,0 +1,16 @@
+package com.hfad.mymessanger;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+    }
+}
